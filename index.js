786fc5cd3cf955a54e1bd89d8df4c5867a0b4079
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2023-12-18 08:25:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Akin"];
+function writeCards(names) {
+  let thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+  let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+
+  thankYouMessages.push(message);
+  }
+ return thankYouMessages;
+}
+console.log(writeCards(names));
+function countDown(number){
+  let  z = 10;
+  while(z>=0){
+    console.log(z);
+    z--;
+  }
+}
+countDown(10);
